66f258480eb7b11849816b1ef690310b7af55105
Kishor <kishorgn@gmail.com>
2022-07-29 16:03:51 +0530

    1.1 - JPA project with layers and exception using Spring Boot

--- src/main/java/com/rebel/SpringBootJpaDemo01Application.java
+++ src/main/java/com/rebel/SpringBootJpaDemo01Application.java
@@ -4,18 +4,29 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
+import com.rebel.exception.InvalidPersonIdException;
 import com.rebel.model.Person;
 import com.rebel.repo.PersonRepo;
+import com.rebel.service.PersonService;
 
 @SpringBootApplication
 public class SpringBootJpaDemo01Application {
 
 	public static void main(String[] args) {
 		ConfigurableApplicationContext ctxt = SpringApplication.run(SpringBootJpaDemo01Application.class, args);
-		PersonRepo personRepo = ctxt.getBean(PersonRepo.class);
-		Person p1 = new Person("Kishor", "kishorgn@gmail.com");
-		Person saved = personRepo.save(p1);
-		System.out.println("Saved person object : "+saved);
+		PersonService personService = ctxt.getBean(PersonService.class);
+//		Person p1 = personService.savePerson(new Person("Ram", "ram@ayodya.com"));
+//		System.out.println("Saved successfully "+p1);
+		
+		Person p1;
+		try {
+			p1 = personService.findPersonById(101);
+			System.out.println(p1);
+		} catch (InvalidPersonIdException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 
 }
